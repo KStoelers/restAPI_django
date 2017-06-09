eb95079b7516d640fe10bdd8a647deafb23a7386
KStoelers <kevin_stoelers@hotmail.com>
2017-06-09 12:37:51 +0200

    typos fixed in models file

--- rest_brameda/brameda/models.py
+++ rest_brameda/brameda/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
-class Customer(models.model):
-	first_name = models.Charfield(max_length=50)
-	last_name = models.Charfield(max_length=50)
-	email = models.Charfield(max_length=50)
-	address = models.Charfield(max_length=50)
+class Customer(models.Model):
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	email = models.CharField(max_length=50)
+	address = models.CharField(max_length=50)
 	
-class Product(models.model):
-	name = models.Charfield(max_length=75)
+class Product(models.Model):
+	name = models.CharField(max_length=75)
 	price = models.DecimalField(max_digits=5, decimal_places=2)
 	product_id = models.IntegerField()
 
-class Order(models.model):
+class Order(models.Model):
 	order_id = models.IntegerField()
 	order_rows = models.IntegerField()
 	
-class OrderRow(models.model):
+class OrderRow(models.Model):
 	order_row_id = models.IntegerField()
 	product = models.IntegerField()
 	amount = models.IntegerField()
